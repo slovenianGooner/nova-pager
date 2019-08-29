dd9bd13e773da853c615ab94302a1b5bb2696fd6
slovenianGooner <lovro.papez@gmail.com>
2019-08-29 20:03:01 +0200

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -7,15 +7,16 @@ if (!function_exists('nova_get_slugs')) {
 
         $formatPages = function (\Illuminate\Database\Eloquent\Collection $pages, $prefix) use (&$formatPages, &$slugs) {
             $pages->each(function ($page) use (&$slugs, $prefix, &$formatPages) {
-                $slugs[$page->id] = ($page->slug !== "/") ? $prefix."/".$page->slug : "/";
-                $children = \Sloveniangooner\NovaPager\Models\Page::where('parent_id', $page->id)->where("name->".app()->getLocale(), "!=", null)->get();
+                $pageSlug = $page->getTranslation("slug", app()->getLocale(), false);
+                $slugs[$page->id] = ($pageSlug !== "/") ? $prefix . "/" . $pageSlug : "/";
+                $children = \Sloveniangooner\NovaPager\Models\Page::where('parent_id', $page->id)->where("name->" . app()->getLocale(), "!=", null)->get();
                 if ($children->count() > 0) {
-                    $formatPages($children, $prefix."/".$page->slug);
+                    $formatPages($children, $prefix . "/" . $pageSlug);
                 }
             });
         };
 
-        $parentPages = \Sloveniangooner\NovaPager\Models\Page::whereNull('parent_id')->where("name->".app()->getLocale(), "!=", null)->get();
+        $parentPages = \Sloveniangooner\NovaPager\Models\Page::whereNull('parent_id')->where("name->" . app()->getLocale(), "!=", null)->get();
         $formatPages($parentPages, "");
         return $slugs;
     }
